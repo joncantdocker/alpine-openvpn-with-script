ca60ca9a6f730943a8e72fc5c9691e0c77462103
joncantdocker <52984868+joncantdocker@users.noreply.github.com>
2019-07-19 12:04:57 +0800

    Adding the files
    Should be all good!

--- serverconfig.sh
+++ serverconfig.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Welcome to the vpn server setup script."
-echo "Due to technical issues you can only run this script once before having to remove a docker container."
+echo "Due to life you can only run this script once before having to remove a docker container."
+echo "To remove the docker container run 'docker container ls -a' to see the running container then docker container stop then rm the container marked rlesouef/alpine-openvpn"
 
-read -p "URL/IP of server? "
+read -p "URL/IP of server? " ServerName
 export OVPN_DATA=openvpn_data
 docker volume create --name $OVPN_DATA
 docker run -v $OVPN_DATA:/etc/openvpn --rm rlesouef/alpine-openvpn initopenvpn -u udp://$ServerName
@@ -17,7 +18,7 @@ echo "note: the server itself is always authorised"
 read -p "Do you want to add a client? (enter nothing to stop adding clients) " Continue
 
 #While Loop for adding muliple clients
-while [ $Continue != "" ]
+while [ "$Continue" != "" ]
 do
 	#select client to authorise
 	read -p "Client name to authorise? (e.g. user1) " ClientName
@@ -34,4 +35,3 @@ do
 	echo "The config settings were saved to the file $ClientName.ovpn "
 	read -p "Do you want to add another client? (enter nothing to stop adding clients) " Continue
 done
-
