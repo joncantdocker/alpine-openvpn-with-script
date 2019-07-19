1bad3632b4db6ff868bf94923f0b5765e72dbd2c
joncantdocker <52984868+joncantdocker@users.noreply.github.com>
2019-07-19 16:01:07 +0800

    hopefully-done
    WORK!!!

--- serverconfig.sh
+++ serverconfig.sh
@@ -7,11 +7,11 @@ read -p "URL/IP of server? " ServerName
 export OVPN_DATA=openvpn_data
 docker volume create --name $OVPN_DATA
 docker run -v $OVPN_DATA:/etc/openvpn --rm joncantdocker/alpine-openvpn-with-script initopenvpn -u udp://$ServerName
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it joncantdocker/alpine-openvpn-with-script initpki
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it rlesouef/alpine-openvpn initpki
 docker run --name openvpn -v $OVPN_DATA:/etc/openvpn -v /etc/localtime:/etc/localtime:ro -d -p 1194:1194/udp --cap-add=NET_ADMIN rlesouef/alpine-openvpn
 
 #view authorised clients
-docker run -v $OVPN_DATA:/etc/openvpn --rm joncantdocker/alpine-openvpn-with-script listcerts
+docker run -v $OVPN_DATA:/etc/openvpn --rm rlesouef/alpine-openvpn listcerts
 
 echo "note: the server itself is always authorised"
 
@@ -24,13 +24,19 @@ do
 	read -p "Client name to authorise? (e.g. user1) " ClientName
 
 	#generate a client certificate
-	docker run -v $OVPN_DATA:/etc/openvpn --rm -it joncantdocker/alpine-openvpn-with-script easyrsa build-client-full $ClientName
+	docker run -v $OVPN_DATA:/etc/openvpn --rm -it rlesouef/alpine-openvpn easyrsa build-client-full $ClientName
 
 	#save the certificate to a file called ClientName.ovpn
-	docker run -v $OVPN_DATA:/etc/openvpn --rm joncantdocker/alpine-openvpn-with-script getclient $ClientName > $ClientName.ovpn
+	docker run -v $OVPN_DATA:/etc/openvpn --rm rlesouef/alpine-openvpn getclient $ClientName > $ClientName.ovpn
 
 	#view authorised clients again
-	docker run -v  $OVPN_DATA:/etc/openvpn --rm joncantdocker/alpine-openvpn-with-script listcerts
+	docker run -v  $OVPN_DATA:/etc/openvpn --rm rlesouef/alpine-openvpn listcerts
+	#remove last 2 lines which are bad
+	head -n -2 $ClientName.ovpn > temp.ovpn ; mv temp.ovpn $ClientName.ovpn
+	#add to the start to have it work
+	echo 'push "route-nopull"' | cat - $ClientName.ovpn > temp.ovpn && mv temp.ovpn $ClientName.ovpn
+	echo 'push "route 172.20.10.3 255.255.255.0 vpn_gateway"' | cat - $ClientName.ovpn > temp.ovpn && mv temp.ovpn $ClientName.ovpn
+
 	#user confirmation
 	echo "The config settings were saved to the file $ClientName.ovpn "
 	read -p "Do you want to add another client? (enter nothing to stop adding clients) " Continue
